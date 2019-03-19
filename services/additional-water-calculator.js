3376c0b7eabf480a3c5e27313f11017e72bc49e3
Alex <zaychenko96y@outlook.com>
2019-03-19 11:35:39 +0200

    Added error handling for calculator

--- services/additional-water-calculator.js
+++ services/additional-water-calculator.js
@@ -10,26 +10,36 @@ const additionalWaterData = {
 
 module.exports = {
   calculateTrueInitDensity (wortTemperature, boilDensity) {
-    wortTemperature = parseInt(wortTemperature)
-    const { initTemperature, degreeStep, degreeDiffPerStep } = additionalWaterData
-    const diff = Math.floor((wortTemperature - initTemperature) / degreeStep)
-    if (diff === 0)
-      return parseInt(boilDensity)
-    for (let i = 0; i < diff; i++) {
-      boilDensity -= degreeDiffPerStep
+    try {
+      wortTemperature = parseInt(wortTemperature)
+      const { initTemperature, degreeStep, degreeDiffPerStep } = additionalWaterData
+      const diff = Math.floor((wortTemperature - initTemperature) / degreeStep)
+      if (diff === 0)
+        return parseInt(boilDensity)
+      for (let i = 0; i < diff; i++) {
+        boilDensity -= degreeDiffPerStep
+      }
+      return boilDensity
     }
-    return boilDensity
+    catch (err) {
+      console.log(err)
+    }
   },
   calculateTruePlannedDensity (brewingTime, plannedDensity) {
-    brewingTime = parseInt(brewingTime)
-    const { initBrewingCoeff, brewingStep, initBrewingTime, brewingDiffPerStep } = additionalWaterData
-    const diff = Math.floor((brewingTime - initBrewingTime) / brewingStep)
-    if (diff === 0)
-      return plannedDensity - initBrewingCoeff
-    let currentBrewingCoeff = initBrewingCoeff
-    for (let i = 0; i < diff; i++) {
-      currentBrewingCoeff += brewingDiffPerStep
+    try {
+      brewingTime = parseInt(brewingTime)
+      const { initBrewingCoeff, brewingStep, initBrewingTime, brewingDiffPerStep } = additionalWaterData
+      const diff = Math.floor((brewingTime - initBrewingTime) / brewingStep)
+      if (diff === 0)
+        return plannedDensity - initBrewingCoeff
+      let currentBrewingCoeff = initBrewingCoeff
+      for (let i = 0; i < diff; i++) {
+        currentBrewingCoeff += brewingDiffPerStep
+      }
+      return plannedDensity - currentBrewingCoeff
     }
-    return plannedDensity - currentBrewingCoeff
+    catch (err) {
+      console.log(err)
+    }
   }
 }
